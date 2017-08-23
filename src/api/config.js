6c6bb6091db6e1120850f3c638e41fcc8cfcd39a
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-08-23 00:44:57 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,13 @@
+
+export default {
+	appname: 'platzimusic',
+	apiKey: '4e31c96a13c22943b9f9439bfe47785c',
+	secret: '5983aebb749c5bfac229c49dfb2f0bb5',
+	RegisteredTo: 'ivanTadeoHuerta'
+}
+
+
+// Application name	platzimusic
+// API key	4e31c96a13c22943b9f9439bfe47785c
+// Shared secret	5983aebb749c5bfac229c49dfb2f0bb5
+// Registered to	ivanTadeoHuerta
